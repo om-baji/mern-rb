34a2b8864682b29bad5a26a519cc62a459da639c
om-baji <ombaji124@gmail.com>
2024-12-10 20:54:10 +0530

    cors f2

--- api/index.ts
+++ api/index.ts
@@ -9,7 +9,7 @@ const app = express();
 app.use(
   cors({
     origin: ["http://localhost:5173","https://mern-rb.vercel.app/"],
-    methods: ["POST", "GET"],
+    methods: ["POST", "GET", "OPTIONS"],
     credentials: true,
   })
 );
